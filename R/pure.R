7b36338a96afc8845e0143191f7424ebba5e4b8d
Rob J Hyndman <robjhyndman@gmail.com>
2025-05-15 15:32:53 +1000

    fetch_pure now working

--- R/pure.R
+++ R/pure.R
@@ -9,61 +9,70 @@
 #'
 #' Publications up to 2024 are available in the \code{\link{ebs_pure}} dataset.
 #'
-#' @param years A numeric vector of publication years.
+#' @param years A numeric vector of publication years. All publications between
+#' the minimum year and the maximum year are returned.
 #' @return A data frame containing the data fetched from the PURE API covering
 #' the specified publication years.
 #' @seealso \code{\link{ebs_pure}}
 #' @export
 
-fetch_pure <- function(years = 2025:2026) {
+fetch_pure <- function(years) {
+  # If we've already fetched this in the current session, just read it from file
+  dest_folder <- tempdir()
+  dest_file <- paste0(dest_folder, "/pure", paste(range(years),collapse="_"), ".rds")
+  if (file.exists(dest_file)) {
+    return(readRDS(dest_file))
+  }
+
   # Check if the API key is set
   if (Sys.getenv("PURE_API_KEY") == "") {
     stop("Please set the PURE_API_KEY environment variable.")
   }
 
-  # Define the API endpoint
-  url <- "https://6wlo3eknv3.execute-api.ap-southeast-2.amazonaws.com/qat/predict/org-map/external"
-
-  # Set the headers
-  headers <- c(
-    "x-api-key" = Sys.getenv("PURE_API_KEY"),
-    "Content-Type" = "application/json"
-  )
-  data <- list(
-    faculty_list = "Faculty of Business & Economics",
-    department_list = "Econometrics & Business Statistics",
-    year_start = 2018,
-    year_end = 2025
+  # Prepare JSON payload
+  payload <- list(
+      faculty_list = list("Faculty of Business & Economics"),
+      department_list = list("Econometrics & Business Statistics"),
+      year_start = min(years),
+      year_end = max(years)
   )
 
-  # Make the GET request
-  response <- httr::GET(url, httr::add_headers(.headers = headers))
+  # Make the API call
+  response <- httr::POST(
+    url = "https://6wlo3eknv3.execute-api.ap-southeast-2.amazonaws.com/qat/predict/org-map/external",
+    config = httr::add_headers(
+      "x-api-key" = Sys.getenv("PURE_API_KEY"),
+      "Content-Type" = "application/json"
+    ),
+    body =jsonlite::toJSON(payload, auto_unbox = TRUE),
+    encode = "raw"
+  )
 
-  # Check for errors
+  # Check if the request was successful
   if (httr::http_error(response)) {
-    stop("Failed to fetch data from PURE API.")
+    stop(
+      sprintf(
+        "API request failed [%s]: %s",
+        httr::status_code(response),
+        httr::http_status(response)$message
+      )
+    )
   }
 
-  # Parse the response
-  data <- httr::content(response, as = "parsed", type = "application/json")
-
-  return(data)
+  # Parse JSON response to R object
+  result <- httr::content(response, "text")
+  result <- jsonlite::fromJSON(result)
+  result <- clean_pure_json(result[[2]])
+  saveRDS(result, dest_file)
+  return(result)
 }
 
-# curl --location 'https://6wlo3eknv3.execute-api.ap-southeast-2.amazonaws.com/qat/predict/org-map/external' \
-# --header 'x-api-key: ?????' \
-# --header 'Content-Type: application/json' \
-# --data '{
-#     "faculty_list":["Faculty of Business & Economics"],
-#     "department_list": ["Econometrics & Business Statistics"],
-#     "year_start": 2018,
-#     "year_end": 2025
-# }'
 
 # Clean PURE json file and return a tibble
 clean_pure_json <- function(json_data) {
-  ebs <- as_tibble(json_data) |>
-    select(
+  browser()
+  ebs <- dplyr::as_tibble(json_data) |>
+    dplyr::select(
       pure_id = output_id,
       year = output_year,
       subtype = output_subtype,
@@ -71,17 +80,17 @@ clean_pure_json <- function(json_data) {
       journal = journal_title,
       bib = output_bibliographic_text_harvard
     ) |>
-    mutate(
+    dplyr::mutate(
       pure_id = as.character(pure_id),
-      doi = str_extract(bib, "doi.org/[0-9a-zA-Z._\\(\\)\\/\\-]*$") |>
-        str_remove("doi.org/") |> str_trim(),
-      authors = str_extract(bib, "^(.*?)(?=\\d)") |> str_trim()
+      doi = stringr::str_extract(bib, "doi.org/[0-9a-zA-Z._\\(\\)\\/\\-]*$") |>
+        stringr::str_remove("doi.org/") |> stringr::str_trim(),
+      authors = stringr::str_extract(bib, "^(.*?)(?=\\d)") |> stringr::str_trim()
     ) |>
-    select(pure_id, year, authors, title, journal, subtype, bib, doi)
+    dplyr::select(pure_id, year, authors, title, journal, subtype, bib, doi)
 
   # Add known missing DOIs
   ebs |>
-    mutate(
-      doi = if_else(pure_id == "617686492", "10.1016/j.jeconom.2022.08.006", doi)
+    dplyr::mutate(
+      doi = dplyr::if_else(pure_id == "617686492", "10.1016/j.jeconom.2022.08.006", doi)
     )
 }
